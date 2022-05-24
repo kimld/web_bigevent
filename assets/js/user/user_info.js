f117f6cb573131f07f95129d5f12f70284509c21
pxx <fangtaun73@163.com>
2022-05-24 18:17:36 +0800

    完成个人中心开发功能

---
+++ assets/js/user/user_info.js
@@ -0,0 +1,60 @@
+$(function(){
+    var form = layui.form
+    var layer=layui.layer
+
+    form.verify({
+        nickname:function(value){
+            if(value.length>6){
+                return '昵称长度必须在1~6个字符之间！'
+            }
+        }
+    })
+    initUserInfo()
+//   初始用户的基本化信息
+function initUserInfo(){
+    $.ajax({
+        method:'GET',
+        url:'/my/userinfo',
+        success:function(res){
+            if(res.status!=0){
+                return layer.msg('获取用户信息失败!')
+            }
+            console.log(res);
+            //调用form.val()快速为表单赋值
+            form.val('formUserInfo',res.data)
+        }
+    })
+}
+
+//重置表单数据
+$('#btnReset').on('click',function(e){
+    // 阻止表单的默认重置行为
+    e.preventDefault()
+    initUserInfo()
+})
+
+$('.layui-form').on('submit',function(e){
+    // 阻止表单默认的提价行为
+    e.preventDefault()
+    // 发起ajax的数据请求
+    $.ajax({
+        method:'POST',
+        url:'/my/userinfo',
+        data:$(this).serialize(),
+        success:function(res){
+            if(res.status!==0){
+                return layer.msg('更新用户信息失败')
+            }
+            layer.msg('更新用户信息成功!')
+            // 调用父页面的方法，重新渲染用户的头像和用户的信息
+            window.parent.getUserInfo()
+        }
+    })
+})
+
+
+
+
+
+
+})
